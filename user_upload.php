2b5d17bfde11ecb5e00a09f15efc1a3f1bca897a
[shuraz] <mrsurajpokhrel@gmail.com>
2021-07-09 12:27:18 +1000

    including db.php script in user_upload

--- user_upload.php
+++ user_upload.php
@@ -1,3 +1,19 @@
 <?php
-echo "hello"
+include 'db.php';
+$conn = getdb();
+$temp_name=$temp_surname=$temp_email='test';
+			
+			$sql = "INSERT INTO users(name,surname,email) VALUES ('".$temp_name."','".$temp_surname."','".$temp_email."')";  
+		
+			if(mysqli_query($conn,$sql)){
+				echo "Record Successfully Inserted.";
+			}
+			else{
+				echo "Record not inserted , ".mysqli_error($conn);
+			}
+		
+		mysqli_close($conn);
+
+
+
 ?>
