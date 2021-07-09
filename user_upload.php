2a119ec8e92b2fcb58aef8ee554f718a3d91a1b2
[shuraz] <mrsurajpokhrel@gmail.com>
2021-07-09 13:31:23 +1000

    Email validation

--- user_upload.php
+++ user_upload.php
@@ -3,27 +3,41 @@
     $conn=getdb();
     $filename = 'users.csv';
     $data = [];
-    $temp_name=$temp_surname=$temp_email='temp';
+    $temp_name=$temp_surname=$temp_email='';
 
 try{
-
+    //opening the file in read only mode
     $holder = fopen($filename, 'r');
+    //using while loop to get all the csv in line by line
     while ($row = fgetcsv($holder, 1000, ",")) {
         $data[] = $row;
     }
-    print_r($data);
+        for ($r = 1; $r < 12; $r++) 
+        {  
+            //string conversion to lower case and first leter capital
+            $temp_name=ucfirst(strtolower($data[$r][0]));
+            $temp_surname=ucfirst(strtolower($data[$r][1])); 
+            $temp_email=strtolower($data[$r][2]);
+            //email validation with filter
+            if (!filter_var($temp_email, FILTER_VALIDATE_EMAIL)) {
+                echo "Invalid email format";
 
+            }
+            else{
 
-    $sql = "INSERT INTO users(name,surname,email) VALUES ('".$temp_name."','".$temp_surname."','".$temp_email."')";  
-	//echo $sql;	
-    if(mysqli_query($conn,$sql)){
-        echo "Record Successfully Inserted.";
+            //querry to insert data in to table
+            $sql = "INSERT INTO users(name,surname,email) VALUES ('".$temp_name."','".$temp_surname."','".$temp_email."')";  
+            //echo $sql;	
+                    
+            if(mysqli_query($conn,$sql)){
+                echo "Record Successfully Inserted.";
+            }
+            else{
+                echo "Record not inserted , ".mysqli_error($conn);
+            }
     }
-    else{
-        echo "Record not inserted , ".mysqli_error($conn);
-    }
-
-    mysqli_close($conn);
+         }
+ mysqli_close($conn);
 
 } catch (Exception $e) {
 	echo $e->getMessage();
